19bb8eebb64c84d374dfcc35ea8c816dd39f64a3
Luis A Garcia <a01540063@itesm.mx>
2020-09-01 22:04:22 -0500

    monthday
    Signed-off-by: Luis A Garcia <a01540063@itesm.mx>

--- labs/month-day/month_day.c
+++ labs/month-day/month_day.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-/* month_day function's prototype*/
 void month_day(int year, int yearday, int *pmonth, int *pday);
+int main(int argc, char ** argv){
 
-int main() {
+    if (argc < 3) {
+    printf("You need to put the correct data\n");
+    return 1;
+  }
+
+    int pmonth = 0;
+    int pday = 0;
+    int year = atoi(argv[1]);
+    int yearday = atoi(argv[2]);
+    month_day(year, yearday, &pmonth, &pday);
+    printf(" %d, %s\n", pday, argv[1]);
     return 0;
+}
+
+void month_day(int year, int yearday, int *pmonth, int *pday){
+    static char daytab[2][13] = {{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},{31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}};
+    static char *months[12] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+
+    int i, leap, previousSum, actualSum;
+    i = leap = previousSum = actualSum = 0;
+    leap = (year%4 == 0 && year%100 != 0) || year%400 ==0;
+
+    for(; yearday > actualSum; i++){
+        previousSum = actualSum;
+        actualSum += daytab[leap][i];
+    }
+    *pday = yearday - prevSum;
+    *pmonth = i;
+
+    printf("%s", months[i-1]);
 }
