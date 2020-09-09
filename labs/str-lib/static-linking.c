9b9727b4cb2b968387c2975ee4dcaf2b343cce41
Luis A Garcia <a01540063@itesm.mx>
2020-09-08 21:16:07 -0500

    Solve strlib
    Signed-off-by: Luis A Garcia <a01540063@itesm.mx>

--- labs/str-lib/static-linking.c
+++ labs/str-lib/static-linking.c
@@ -1,5 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
+int mystrlen(char *);
+char *mystradd(char *, char *);
+int mystrfind(char *, char *);
+
+int main(int argc, char* argv[]) {
+
+    if (argc != 4){
+        printf("Please use correct arguments. \n");
+        return 0;
+    }
+
+    if(strcmp(argv[1], "-add") == 0){
+        int len = mystrlen(argv[2]);
+        char* newStr = mystradd(argv[2], argv[3]);
+        int newLen = mystrlen(newStr);
+        printf("Initial Length      : %i\n", len);
+        printf("New String             : %s\n", newStr);
+        printf("New Length          : %i\n", newLen);
+    }else if(strcmp(argv[1], "-find") == 0){
+        int pos = mystrfind(argv[2], argv[3]);
+        printf("['%s'] string was found at [%i] position\n", argv[3], pos);
+    }else{
+        printf("Please use -add or -find as the second argument\n");
+        return 0;
+    }
+
     return 0;
 }
