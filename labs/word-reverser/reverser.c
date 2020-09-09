8368d42ff9a3131a089afd681013263d86051927
Luis A Garcia <a01540063@itesm.mx>
2020-09-09 14:59:12 -0500

    Good word reverser
    Signed-off-by: Luis A Garcia <a01540063@itesm.mx>

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -2,5 +2,22 @@
 
 int main(){
     // Place your magic here
+    char c;
+    char word[100];
+
+    int i =0;
+    while ((c = getchar()) != EOF) {
+      if (c== '\n') {
+        i--;
+        while (i>= 0) {
+          printf("%c", word[i--]);
+        }
+        i = 0;
+        printf("\n");
+      }
+      else{
+        word[i++] = c;
+      }
+    }
     return 0;
 }
