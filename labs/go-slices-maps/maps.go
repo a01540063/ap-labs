f6ea54aae11b6135017d744a44323c17b5e2d67e
Luis A Garcia <a01540063@itesm.mx>
2020-09-22 19:34:42 -0500

    go-slices-maps
    Signed-off-by: Luis A Garcia <a01540063@itesm.mx>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	m:=make(map[string]int)
+	words := strings.SplitAfter(s," ")
+	for i:=0;i<len(words);i++{
+		m[strings.Trim(words[i]," ")]+=1
+	}
+	return m
+}
+
 func main() {
+	wc.Test(WordCount)
 }
