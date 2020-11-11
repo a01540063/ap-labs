80f95bca43fb9d27841b0642d2dceb969c2d6d05
Luis A Garcia <a01540063@itesm.mx>
2020-11-10 22:08:53 -0600

    goroutines-vs-osthreads
    Signed-off-by: Luis A Garcia <a01540063@itesm.mx>

--- labs/goroutines-vs-os-threads/ping-pong.go
+++ labs/goroutines-vs-os-threads/ping-pong.go
@@ -2,10 +2,44 @@ package main
 
 import (
 	"fmt"
+	"time"
+
 )
 
 func main() {
-	var commsPerSecond int
+	var pingpongCount int;
+	ping := make(chan int)
+	pong := make(chan int)
+	done := make(chan struct{})
+  	pingpongCount = 1000000
 
-	fmt.Println("Communications Per Second : ", commsPerSecond)
+	startTime := time.Now()
+
+	go func() {
+
+		for n := 0; n < pingpongCount; n++ {
+			ping <- n
+			<-pong
+		}
+
+		close(ping)
+		close(done)
+	}()
+
+	go func() {
+
+		for n := range ping {
+			pong <- n
+		}
+
+		close(pong)
+	}()
+
+	<-done
+	
+	endTime := time.Now()
+	deltaT := endTime.Sub(startTime)
+	time := float64(deltaT.Nanoseconds()) / 1000000000.0
+	rate := float64(pingpongCount) / time
+	fmt.Printf("Elapsed time: %v \t Number of messages: %v Number of Replies: \t%f \n", deltaT, pingpongCount, rate)
 }
--- labs/goroutines-vs-os-threads/pipeline.go
+++ labs/goroutines-vs-os-threads/pipeline.go
@@ -1,12 +1,46 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"time"
+	"strconv"
 )
 
 func main() {
-	var maxStages, transitTime int
+	pipes := flag.Int("pipes", 1000000, "Number of pipes")
+	verbose := flag.Bool("verbose", false, "Printing created pipes")
+	flag.Parse()
+	var start time.Time
+	ch := make(chan struct{})
+	in := ch
+	start = time.Now()
 
-	fmt.Println("Maximum number of pipeline stages   : ", maxStages)
-	fmt.Println("Time to transit trough the pipeline : ", transitTime)
+	for i := 1; i <= *pipes; i++ {
+		out := make(chan struct{})
+		go func(in <-chan struct{}, out chan<- struct{}, i int) {
+			out <- <-in
+		}(in, out, i)
+
+		in = out
+		if *verbose {
+			fmt.Printf("\r[%d] ", i)
+		}
+	}
+
+	if *verbose {
+		fmt.Println()
+	}
+
+	fmt.Printf("Goroutines created in %v\n", time.Since(start))
+	start = time.Now()
+	ch <- struct{}{}
+	<-in
+	fmt.Printf("Message transmitted in %v\n", time.Since(start))
+
+
+  tme := time.Since(start)
+  str := "Number of goroutines created: "+strconv.Itoa(*pipes)+"\nIt took "+tme.String() +" to transmit the message\n"
+  fmt.Printf(str)
+
 }
